b59fa132085f51f8179cbda6dab7fc092386b116
s-hesam-s <hesamshoushtarian@gmail.com>
2024-01-26 23:02:08 +0330

    Section 4 - Use Connection String

--- MagicVilla_VillaAPI/Program.cs
+++ MagicVilla_VillaAPI/Program.cs
@@ -1,4 +1,6 @@
+using MagicVilla_VillaAPI.Data;
 using MagicVilla_VillaAPI.Logging;
+using Microsoft.EntityFrameworkCore;
 using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,6 +10,10 @@ var builder = WebApplication.CreateBuilder(args);
 //Log.Logger = new LoggerConfiguration().MinimumLevel.Debug().WriteTo
 //    .File("log/villalogs.txt",rollingInterval:RollingInterval.Day).CreateLogger();
 //builder.Host.UseSerilog();
+
+builder.Services.AddDbContext<ApplicationDbContext>(option => {
+    option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultSQLConnection"));
+});
 
 builder.Services.AddControllers(option => {
     //option.ReturnHttpNotAcceptable = true;
