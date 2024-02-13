01bbcbb1edfa29cfc0d8bcedcdc83d8ee9b1e550
s-hesam-s <hesamshoushtarian@gmail.com>
2024-02-13 20:55:50 +0330

    Section 7 - Create Villa Number GET

--- MagicVilla_Web/Controllers/VillaNumberController.cs
+++ MagicVilla_Web/Controllers/VillaNumberController.cs
@@ -1,8 +1,10 @@
 ﻿using AutoMapper;
 using MagicVilla_Web.Models;
 using MagicVilla_Web.Models.Dto;
+using MagicVilla_Web.Models.VM;
 using MagicVilla_Web.Services.IServices;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Newtonsoft.Json;
 
 namespace MagicVilla_Web.Controllers
@@ -10,10 +12,13 @@ namespace MagicVilla_Web.Controllers
     public class VillaNumberController : Controller
     {
         private readonly IVillaNumberService _villaNumberService;
+        private readonly IVillaService _villaService;
         private readonly IMapper _mapper;
-        public VillaNumberController(IVillaNumberService villaNumberService, IMapper mapper)
+        public VillaNumberController(IVillaNumberService villaNumberService, IVillaService villaService,
+            IMapper mapper)
         {
             _villaNumberService = villaNumberService;
+            _villaService = villaService;
             _mapper = mapper;
         }
 
@@ -29,5 +34,23 @@ namespace MagicVilla_Web.Controllers
 
             return View(list);
         }
+
+        public async Task<IActionResult> CreateVillaNumber()
+        {
+            VillaNumberCreateVM villaNumberVM = new();
+            var response = await _villaService.GetAllAsync<APIResponse>();
+            if (response != null && response.IsSuccess)
+            {
+                villaNumberVM.VillaList = JsonConvert.DeserializeObject<List<VillaDTO>>
+                    (Convert.ToString(response.Result)).Select(i => new SelectListItem
+                    {
+                        Text = i.Name,
+                        Value = i.Id.ToString()
+                    }); ;
+            }
+
+            return View(villaNumberVM);
+        }
+
     }
 }
